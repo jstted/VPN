a316b5f35ecc99694cefb1ecc8c23d3a06f23a3e
Mikhail <tedeev.mikhaill@gmail.com>
2024-06-08 18:40:40 +0300

    Initial Commit

---
+++ VPNnotif/VPNnotifApp.swift
@@ -0,0 +1,17 @@
+//
+//  VPNnotifApp.swift
+//  VPNnotif
+//
+//  Created by Mikhail Tedeev on 08.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct VPNnotifApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
